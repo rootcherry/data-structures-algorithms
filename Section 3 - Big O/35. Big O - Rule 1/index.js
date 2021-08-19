a83b72a3ac4764742d4aa33725ad224c032fdb71
Chris N <hi.chrisn@gmail.com>
2021-08-19 15:21:26 -0300

    big o rule 1

---
+++ Section 3 - Big O/35. Big O - Rule 1/index.js
@@ -0,0 +1,21 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(100000).fill('nemo');
+const performance = require('perf_hooks').performance;
+
+function findNemo(array) {
+    let t0 = performance.now();
+    for (let i = 0; i < array.length; i++) {
+        console.log('running');
+        if (array[i] === 'nemo') {
+            console.log('Found NEMO!');
+            break;
+        }
+    }
+    let t1 = performance.now();
+    console.log('Call to find Nemo took ' + (t1 - t0) + ' miliseconds');
+}
+
+// findNemo(nemo);
+findNemo(everyone);
+// findNemo(large);
