aa7fecf92dde70c27d0f6a626da743c864bfb706
Chris N <hi.chrisn@gmail.com>
2021-08-19 14:30:48 -0300

    solution big o calculation

--- Section 3 - Big O/30. Exercise - Big O Calculation/index.js
+++ Section 3 - Big O/30. Exercise - Big O Calculation/index.js
@@ -1,11 +1,16 @@
 function funChallenge(input) {
-    let a = 10;
-    a = 50 + 3;
+    let a = 10; // O(1)
+    a = 50 + 3; //  O(1)
 
-    for (let i = 0; i < input.length; i++) {
-        anotherFunction();
-        let strange = true;
-        a++;
+    for (let i = 0; i < input.length; i++) { // O(n)
+        anotherFunction(); // O(n)
+        let strange = true; // O(n)
+        a++; // O(n)
     }
-    return a;
+    return a; // O(1)
 }
+
+// funChallenge();
+
+// Big O Notation
+// BIG O(3 + 4n)
