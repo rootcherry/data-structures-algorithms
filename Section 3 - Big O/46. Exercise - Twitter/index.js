d0e8c33b6078756dd320dca66585e9cbca3dec55
Chris N <hi.chrisn@gmail.com>
2021-08-19 21:29:22 -0300

    exercise - twitter

---
+++ Section 3 - Big O/46. Exercise - Twitter/index.js
@@ -0,0 +1,18 @@
+// Find 1st, Find last...
+
+// const array = ['hi', 'my', 'teddy'];
+// array[0]; // O(1)
+// array[array.length - 1]; // O(1)
+
+const array = [{
+    tweet: 'hi',
+    date: 2012
+}, {
+    tweet: 'my',
+    date: 2014
+}, {
+    tweet: 'teddy',
+    date: 2018
+}];
+
+// 'heeeelllloooo!!'.length -> 15 O(1) in JS
