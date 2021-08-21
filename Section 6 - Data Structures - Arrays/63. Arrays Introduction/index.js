94c8a249bdb9f505f3caa0a0db9ec64e8cdc2412
Chris N <hi.chrisn@gmail.com>
2021-08-20 22:02:01 -0300

    update arrays intro

--- Section 6 - Data Structures - Arrays/63. Arrays Introduction/index.js
+++ Section 6 - Data Structures - Arrays/63. Arrays Introduction/index.js
@@ -1,7 +1,9 @@
 const strings = ['a', 'b', 'c', 'd'];
 // 4 * 4 = 16 bytes of storage
+// Variable array is somewhere in memory and the computer knows it.
+// When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
 
-console.log(strings[2]);
+console.log(strings[2]); // O(1)
 
 // array methods
 
@@ -11,5 +13,11 @@ strings.push('e'); // O(1)
 // pop - remove the last item from the array
 strings.pop();
 strings.pop(); // O(1)
+
+// unshift - add an item at the beginning
+strings.unshift('x'); // O(n)
+
+// splice - add an item in the middle
+strings.splice(2, 0, 'alien'); // O(n)
 
 console.log(strings);
