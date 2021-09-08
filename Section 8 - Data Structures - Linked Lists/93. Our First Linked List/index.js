ea89ccabdf0ceaefab03e55f9d47122a7f08f721
Chris N <hi.chrisn@gmail.com>
2021-09-08 14:34:47 -0300

    added getElementByIndex and insert index pos

--- Section 8 - Data Structures - Linked Lists/93. Our First Linked List/index.js
+++ Section 8 - Data Structures - Linked Lists/93. Our First Linked List/index.js
@@ -48,15 +48,55 @@ class LinkedList {
         this.length++;
         return this;
     }
+    // 1 --> 10 --> *99 --> 5 --> 16 (2, 99)
+    insert(index, value) {
+        // check params
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        const node = new Node(value);
+        const leader = this.traverseToIndex(index - 1)
+        const holdingPointer = leader.next;
+        leader.next = node;
+        node.next = holdingPointer;
+        this.length++;
+        return this.printList();    
+    }
 
+    remove(index) {
+
+    }
+
+    traverseToIndex(index) {
+        // check params
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter != index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        // return array;
+        console.log(array);
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-
-console.log(myLinkedList);
-
 myLinkedList.prepend(1);
-
-console.log(myLinkedList);
+myLinkedList.printList();
+// 1 --> 10 --> *99 --> 5 --> 16 (2, 99)
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.printList();
+// console.log(myLinkedList);
