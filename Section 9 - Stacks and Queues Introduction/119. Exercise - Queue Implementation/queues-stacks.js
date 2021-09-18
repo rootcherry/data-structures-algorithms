4eb8ce68e6c87bbfbcfdf14d0c851fd39867c335
Chris N <hi.chrisn@gmail.com>
2021-09-17 22:16:42 -0300

    added queues using stacks

---
+++ Section 9 - Stacks and Queues Introduction/119. Exercise - Queue Implementation/queues-stacks.js
@@ -0,0 +1,44 @@
+class QueueStack {
+    constructor() {
+        this.fisrt = [];
+        this.last = [];
+    }
+
+    peek() {
+        if (this.last.length > 0) return this.last[0];
+        return this.first[this.first.length - 1];
+    }
+
+    enqueue(value) {
+        const length = this.first.length;
+        for (let i = 0; i < length; i++) {
+          this.last.push(this.first.pop());
+        }
+        this.last.push(value);
+        return this;
+    }
+    
+    dequeue() {
+        const length = this.last.length;
+        for (let i = 0; i < length; i++) {
+          this.first.push(this.last.pop());
+        }
+        this.first.pop();
+        return this;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
